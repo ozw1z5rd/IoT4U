d721567a50c4c020a89fe4631273a6003ce97b63
Alessio Palma <ozw1z5rd@gmail.com>
2015-12-17 23:58:26 +0100

    now with image filtering

--- ML/splitCat.m
+++ ML/splitCat.m
@@ -14,7 +14,9 @@ top = 1; rows = 5; cols = 6;
 SIZE_Y = [ 198 213 213 213 186  ];
 
 catsGrid = imread("neko.orig.jpg");
+colormap("gray");
 
+trainingSet = zeros( 30, 1600 );
 for i = 1:rows 
   size_y = SIZE_Y(i);
   for j = 1:cols
@@ -22,26 +24,29 @@ for i = 1:rows
     left = 1+(j-1)*size_x;
     img = catsGrid(top:top+size_y,left:left+size_x,:);
     % make all the cat the same size.
-    imgR = imresize( img, [ 213, 213 ] );
-    imagesc(imgR);
-    
+    imgR = imresize( img, [ 40, 40 ] );
+     
     % convert to gray scale
     [ X, MAP ] = rgb2ind( imgR );
     imgGS = ind2gray( X, MAP );
-    pause(0.1);
         
     % edge detection
-    imgGSE = edge( imgGS, "Canny");
-    imagesc( imgGSE );
-    pause(0.5);
+    %imgGSE = edge( imgGS, "Sobel");
     
-    maxValue = max(max(imgGS));
+    % original picture
+    imagesc( imgR );
+    pause(0.1);
+    % grayscale version
+    imagesc( imgGS );
+    pause(0.1);
+    % decrease colors
+    imgMASK = imgGS/1024; % + imgGSE * 8; 
     
-    imgGSE = maxValue * imgGSE;
-    im = ( imgGS + imgGSE ) / 2 / maxValue;
-    
-    imagesc( im );
-    pause(1);
+    imagesc( imgMASK );
+    pause( 0.1 );
+   
+    % questa e' la matrice con i dati del training set
+    trainingSet(index, : ) = reshape(imgMASK, [ 1, 1600 ] );
     
   endfor
   top += size_y;
