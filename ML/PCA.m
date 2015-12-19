5d28c7f1ee9fcd18cdc1f2bdd7e8fe240bfb3518
Alessio Palma <ozw1z5rd@gmail.com>
2015-12-19 16:50:42 +0100

    Added PCA function, changed the pca demo file, added data visualization to cats'experiment.

---
+++ ML/PCA.m
@@ -0,0 +1,24 @@
+## usage: [V,S] = PCA(X)
+##
+##  X = data set
+##  V = vectors which defines the new space
+##  S = percentage of variance at dimension j
+##
+## Function description
+##
+## Example:
+##
+##  [V,S] = PCA(X);
+##  % decrease features to 3 
+##  X * V(:,1:3) 
+##  % saved variance is S(3) percent
+##  S(3)*100
+##
+function [S, V] = PCA( X ) 
+  s = std(X); s( find(s == 0 ) ) = 1;
+  n = size( X ,1 );
+  X0 = (X - mean(X))./s;
+  C = X0' * X0 / (  n - 1 );
+  [ U,S,V ] = svd( C );
+  S = diag(S)/sum(diag(S));
+endfunction
